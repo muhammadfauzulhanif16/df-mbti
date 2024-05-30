81e93edf72bd52acdbe13f7aaadc6b3980a9db28
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-05-30 23:02:57 +0700

    update

--- resources/js/Pages/Choice/Edit.jsx
+++ resources/js/Pages/Choice/Edit.jsx
@@ -1,8 +1,68 @@
-const Edit = () => {
+import React from 'react'
+import { useForm } from '@inertiajs/react'
+import { Button, Center, NumberInput, TextInput, Title } from '@mantine/core'
+import { router } from '@inertiajs/core'
+import { AppLayout } from '@/Layouts/AppLayout.jsx'
+
+const Edit = (props) => {
+  const form = useForm({
+    name: props.choice.name,
+    value: props.choice.value
+  })
+  console.log(props)
   return (
-    <div>
-      <h1>edit choice</h1>
-    </div>
+    <AppLayout title="Tambah Jawaban" activeNav="Jawaban"
+               authed={props.auth.user} meta={props.meta}>
+      <Center h="100vh" p={16}>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          form.put(route('choices.update', props.choice.id))
+        }}>
+          <Title align="center" mb={32}>Tambah Jawaban</Title>
+          
+          <TextInput
+            withAsterisk
+            label="Nama Jawaban"
+            placeholder="Masukkan nama jawaban..."
+            mb={16}
+            onChange={(e) => form.setData('name', e.target.value)}
+            value={form.data.name}
+          />
+          
+          <NumberInput
+            withAsterisk
+            hideControls
+            label="Bobot Penilaian"
+            placeholder="Masukkan bobot penilaian..."
+            onChange={(value) => form.setData('value', value.toString())}
+            value={form.data.value}
+          />
+          
+          <Button.Group mt={32}>
+            <Button
+              variant="outline"
+              color="red"
+              disabled={form.processing}
+              fullWidth
+              onClick={() => router.get(route('choices.index'))}
+            >
+              Batal
+            </Button>
+            <Button
+              fullWidth
+              disabled={
+                form.data.name === '' ||
+                form.data.value === ''
+              }
+              loading={form.processing}
+              type="submit"
+            >
+              Simpan
+            </Button>
+          </Button.Group>
+        </form>
+      </Center>
+    </AppLayout>
   )
 }
 
--- resources/js/Pages/Choice/Index.jsx
+++ resources/js/Pages/Choice/Index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Group, Stack, Table, TextInput, Tooltip } from '@mantine/core'
-import { IconPlus, IconSearch } from '@tabler/icons-react'
+import { Button, SimpleGrid, Stack, Table, TextInput } from '@mantine/core'
+import { IconPlus, IconUser } from '@tabler/icons-react'
 import { router } from '@inertiajs/core'
 import { AppLayout } from '@/Layouts/AppLayout.jsx'
 
@@ -17,26 +17,24 @@ const Index = (props) => {
     <AppLayout title="Jawaban" activeNav="Jawaban"
                authed={props.auth.user} meta={props.meta}>
       <Stack p={16}>
-        <Group justify="space-between">
-          <Tooltip
-            disabled={!!props.statements.length}
-            label={!props.statements.length && 'Harap isi data pertanyaan dahulu!'}>
-            <Button
-              disabled={!props.statements.length}
-              leftSection={<IconPlus />}
-              onClick={() => router.get(route('choices.create'))}
-            >
-              Tambah Jawaban
-            </Button>
-          </Tooltip>
-          
+        <SimpleGrid cols={{
+          base: 1,
+          xs: 2
+        }} justify="space-between">
           <TextInput
-            leftSection={<IconSearch />}
+            leftSection={<IconUser />}
             placeholder="Cari jawaban..."
             value={search}
             onChange={(event) => setSearch(event.currentTarget.value)}
           />
-        </Group>
+          
+          <Button
+            leftSection={<IconPlus />}
+            onClick={() => router.get(route('choices.create'))}
+          >
+            Tambah Jawaban
+          </Button>
+        </SimpleGrid>
         
         <Table.ScrollContainer>
           <Table horizontalSpacing="xl" verticalSpacing="sm" highlightOnHover
