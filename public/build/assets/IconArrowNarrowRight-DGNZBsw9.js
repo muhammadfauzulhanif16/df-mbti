43ddd57cac30a6a0ac47a341cdcca0722db1ec92
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-07-02 22:18:35 +0700

    update

---
+++ public/build/assets/IconArrowNarrowRight-DGNZBsw9.js
@@ -0,0 +1,11 @@
+import{c as r}from"./AppLayout-D9-29hHX.js";/**
+ * @license @tabler/icons-react v3.6.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var a=r("outline","arrow-narrow-left","IconArrowNarrowLeft",[["path",{d:"M5 12l14 0",key:"svg-0"}],["path",{d:"M5 12l4 4",key:"svg-1"}],["path",{d:"M5 12l4 -4",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.6.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=r("outline","arrow-narrow-right","IconArrowNarrowRight",[["path",{d:"M5 12l14 0",key:"svg-0"}],["path",{d:"M15 16l4 -4",key:"svg-1"}],["path",{d:"M15 8l4 4",key:"svg-2"}]]);export{a as I,t as a};
