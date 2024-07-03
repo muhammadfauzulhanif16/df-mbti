de1653f9b6bf7dfb2ee00c4d436c4c912ce5e851
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-07-03 22:17:56 +0700

    update

--- public/build/assets/IconFileSpreadsheet-CGmY5yso.js
+++
@@ -1,6 +0,0 @@
-import{r as n,u as k,i as R,j as s,k as F}from"./app-Bi4iRz3i.js";import{c as x}from"./AppLayout-DvVrcIVI.js";const M={multiple:!1},C=n.forwardRef((o,l)=>{const{onChange:r,children:i,multiple:a,accept:p,name:c,form:u,resetRef:h,disabled:f,capture:d,inputProps:m,...g}=k("FileButton",M,o),t=n.useRef(),v=()=>{var e;!f&&((e=t.current)==null||e.click())},y=e=>{r(a?Array.from(e.currentTarget.files):e.currentTarget.files[0]||null)};return R(h,()=>{t.current.value=""}),s.jsxs(s.Fragment,{children:[i({onClick:v,...g}),s.jsx("input",{style:{display:"none"},type:"file",accept:p,multiple:a,onChange:y,ref:F(l,t),name:c,form:u,capture:d,...m})]})});C.displayName="@mantine/core/FileButton";/**
- * @license @tabler/icons-react v3.6.0 - MIT
- *
- * This source code is licensed under the MIT license.
- * See the LICENSE file in the root directory of this source tree.
- */var P=x("outline","file-spreadsheet","IconFileSpreadsheet",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}],["path",{d:"M8 11h8v7h-8z",key:"svg-2"}],["path",{d:"M8 15h8",key:"svg-3"}],["path",{d:"M11 11v7",key:"svg-4"}]]);export{C as F,P as I};
