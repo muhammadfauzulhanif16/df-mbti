e90e6a63decd060b2926896f904c8f9bc08a6d50
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-08-18 07:23:02 +0700

    update

---
+++ public/build/assets/IconPhone-IKC2OmFr.js
@@ -0,0 +1,11 @@
+import{c as a}from"./AppLayout-DHrDqh_3.js";/**
+ * @license @tabler/icons-react v3.6.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o=a("outline","id","IconId",[["path",{d:"M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v10a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z",key:"svg-0"}],["path",{d:"M9 10m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-1"}],["path",{d:"M15 8l2 0",key:"svg-2"}],["path",{d:"M15 12l2 0",key:"svg-3"}],["path",{d:"M7 16l10 0",key:"svg-4"}]]);/**
+ * @license @tabler/icons-react v3.6.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=a("outline","phone","IconPhone",[["path",{d:"M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2",key:"svg-0"}]]);export{o as I,t as a};
